52adef1e56880f722aa8e2c4d73d70ed2febe35d
AlbertZ <68438847+redbullgenz@users.noreply.github.com>
2024-10-19 07:13:17 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -10,13 +10,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAu8UNkWm8zCjmZYsELdYb934_YU06qWrI",
-  authDomain: "loginbw-5750b.firebaseapp.com",
-  projectId: "loginbw-5750b",
-  storageBucket: "loginbw-5750b.appspot.com",
-  messagingSenderId: "45104339981",
-  appId: "1:45104339981:web:dd552b4ec50cd197ab4944",
-  measurementId: "G-V9XHKDNVX8"
+  no api key here 
 };
 
 // Initialize Firebase
